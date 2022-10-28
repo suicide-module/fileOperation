3c9f65721ac225bba31ed8b538f4a6ad7e81b6e2
Onur YILDIZ <52601509+onyor@users.noreply.github.com>
2022-10-29 01:20:25 +0300

    FileOperation updated.

--- fileOperation.go
+++ fileOperation.go
@@ -6,16 +6,10 @@ import (
 	"github.com/golang-module/carbon/v2"
 )
 
-func WriteFromFile(date string) {
+func WriteDateToFile(date string) {
 	d1 := []byte(date)
 	err := os.WriteFile("date", d1, 0644)
 	check(err)
-}
-
-func ReadFromFile() string {
-	result, err := os.ReadFile("date")
-	check(err)
-	return string(result)
 }
 
 func ReadDateFromFile() carbon.Carbon {
