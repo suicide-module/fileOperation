d46e3da3f9994ed9fec4e0aec67ac743b623bd2e
Onur YILDIZ <52601509+onyor@users.noreply.github.com>
2022-10-29 01:30:54 +0300

    MonthCompare added.

--- fileOperation.go
+++ fileOperation.go
@@ -23,3 +23,36 @@ func check(e error) {
 		panic(e)
 	}
 }
+
+func MonthCompare(month string) string {
+	var result string
+
+	switch month {
+	case "Ocak":
+		result = "1"
+	case "Şubat":
+		result = "2"
+	case "Mart":
+		result = "3"
+	case "Nisan":
+		result = "4"
+	case "Mayıs":
+		result = "5"
+	case "Haziran":
+		result = "6"
+	case "Temmuz":
+		result = "7"
+	case "Ağustos":
+		result = "8"
+	case "Eylül":
+		result = "9"
+	case "Ekim":
+		result = "10"
+	case "Kasım":
+		result = "11"
+	case "Aralık":
+		result = "12"
+	}
+
+	return result
+}
